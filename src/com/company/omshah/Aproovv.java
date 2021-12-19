fbf907ca111a667495f6ec76978ce6f24fb36a64
om4shah <om4shah@gmail.com>
2021-12-18 23:07:53 -0600

    first commit

---
+++ src/com/company/omshah/Aproovv.java
@@ -0,0 +1,11 @@
+package com.company.omshah;
+
+import static java.lang.System.out;
+
+public class Aproovv {
+
+    public static void main(String[] args)
+    {
+        out.println("Hello bebe");
+    }
+}
